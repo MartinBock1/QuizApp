6c8d07757c23c881334559a7ab683411c478bf96
MB-PC\ich <martinbock@arcor.de>
2024-11-20 17:18:12 +0100

    "final fixes code cleaned"

---
+++ assets/scripts/db.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer entwickelte die Relationale Datenbank?",
+        "answer_1": "Bill Gates",
+        "answer_2": "E.F. Codd",
+        "answer_3": "Steve Jobs",
+        "answer_4": "Mark Zuckerberg",
+        "right_answer": 2
+    },
+    {
+        "question": "Wer erfand die Programmiersprache Python?",
+        "answer_1": "Guido van Rossum",
+        "answer_2": "James Gosling",
+        "answer_3": "Bjarne Stroustrup",
+        "answer_4": "Dennis Ritchie",
+        "right_answer": 1
+    },
+    {
+        "question": "Wer ist der Schöpfer von Linux?",
+        "answer_1": "Linus Torvalds",
+        "answer_2": "Bill Gates",
+        "answer_3": "Steve Wozniak",
+        "answer_4": "Mark Shuttleworth",
+        "right_answer": 1
+    },
+    {
+        "question": "Wer entwickelte die erste Version von Windows?",
+        "answer_1": "Steve Jobs",
+        "answer_2": "Bill Gates",
+        "answer_3": "Larry Page",
+        "answer_4": "Tim Berners-Lee",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet CSS?",
+        "answer_1": "Cascading Style System",
+        "answer_2": "Computer Style Sheets",
+        "answer_3": "Creative Style Systems",
+        "answer_4": "Cascading Style Sheets",
+        "right_answer": 4
+    },
+    {
+        "question": "Was bedeutet HTTP?",
+        "answer_1": "Hyperlink Transfer Protocol",
+        "answer_2": "High Text Transfer Protocol",
+        "answer_3": "Hypertext Transfer Protocol",
+        "answer_4": "Hypertext Transmission Protocol", 
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet URL?",
+        "answer_1": "Uniform Resource Locator",
+        "answer_2": "Universal Resource Locator",
+        "answer_3": "Uniform Reference Locator",
+        "answer_4": "Universal Reference Locator",
+        "right_answer": 1
+    },
+    {
+        "question": "Was bedeutet API?",
+        "answer_1": "Application Programming Integration",
+        "answer_2": "Application Programming Interface", 
+        "answer_3": "Application Protocol Interface",
+        "answer_4": "Application Program Interface",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bedeutet SQL?",
+        "answer_1": "Structured Query Language",
+        "answer_2": "Standard Query Language",
+        "answer_3": "Simple Query Language",
+        "answer_4": "Structured Question Language",
+        "right_answer": 1
+    }
+];
--- script.js
+++ script.js
@@ -1,112 +1,63 @@
-let questions = [
-    {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": 3
-    },
-    {
-        "question": "Wer entwickelte die Relationale Datenbank?",
-        "answer_1": "Bill Gates",
-        "answer_2": "E.F. Codd",
-        "answer_3": "Steve Jobs",
-        "answer_4": "Mark Zuckerberg",
-        "right_answer": 2
-    },
-    {
-        "question": "Wer erfand die Programmiersprache Python?",
-        "answer_1": "Guido van Rossum",
-        "answer_2": "James Gosling",
-        "answer_3": "Bjarne Stroustrup",
-        "answer_4": "Dennis Ritchie",
-        "right_answer": 1
-    },
-    {
-        "question": "Wer ist der Schöpfer von Linux?",
-        "answer_1": "Linus Torvalds",
-        "answer_2": "Bill Gates",
-        "answer_3": "Steve Wozniak",
-        "answer_4": "Mark Shuttleworth",
-        "right_answer": 1
-    },
-    {
-        "question": "Wer entwickelte die erste Version von Windows?",
-        "answer_1": "Steve Jobs",
-        "answer_2": "Bill Gates",
-        "answer_3": "Larry Page",
-        "answer_4": "Tim Berners-Lee",
-        "right_answer": 2
-    },
-    {
-        "question": "Was bedeutet CSS?",
-        "answer_1": "Cascading Style Sheets",
-        "answer_2": "Computer Style Sheets",
-        "answer_3": "Creative Style Systems",
-        "answer_4": "Cascading Style System",
-        "right_answer": 1
-    },
-    {
-        "question": "Was bedeutet HTTP?",
-        "answer_1": "Hypertext Transfer Protocol",
-        "answer_2": "High Text Transfer Protocol",
-        "answer_3": "Hyperlink Transfer Protocol",
-        "answer_4": "Hypertext Transmission Protocol",
-        "right_answer": 1
-    },
-    {
-        "question": "Was bedeutet URL?",
-        "answer_1": "Uniform Resource Locator",
-        "answer_2": "Universal Resource Locator",
-        "answer_3": "Uniform Reference Locator",
-        "answer_4": "Universal Reference Locator",
-        "right_answer": 1
-    },
-    {
-        "question": "Was bedeutet API?",
-        "answer_1": "Application Programming Interface",
-        "answer_2": "Application Programming Integration",
-        "answer_3": "Application Protocol Interface",
-        "answer_4": "Application Program Interface",
-        "right_answer": 1
-    },
-    {
-        "question": "Was bedeutet SQL?",
-        "answer_1": "Structured Query Language",
-        "answer_2": "Standard Query Language",
-        "answer_3": "Simple Query Language",
-        "answer_4": "Structured Question Language",
-        "right_answer": 1
-    }
-];
+
 
 let currentQuestion = 0;
+let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('assets/sounds/success.mp3');
+let AUDIO_FAIL = new Audio('assets/sounds/fail.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
     showQuestion();
 }
 
+function showQuestion() {
+
+    if (gameIsOver()) {
+        showEndScreen();
+    } else {
+        updateProgressBar();
+        updateToNextQuestion();
+    }
+}
+
+/**
+ * @returns 'true' oder 'false'
+ */
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+function showEndScreen() {
+    // Show End-Screen
+    document.getElementById('end-screen').style = '';                   // end-screen anzeigen
+    document.getElementById('question-body').style = 'display: none';   // question-body ausblenden
+
+    document.getElementById('amount-of-questions').innerHTML = questions.length;        // anzeige der menge der fragen im html
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;    // anzeige der menge der richtig beantworteten fragen im html
+    document.getElementById('header-image').src = 'assets/img/trophy.png';              // austausch des bildes wenn quiz zu ende ist
+}
+
 /**
  * wir nehmen das erste element aus dem array und speichern es in die variable 'question'
  * die antwortcontainer werden mit den werten aus dem objekt befüllt
+ * die zahl im footer welche frage angezeigt wird, wird um 1 erhöht (weil default = 0)
  * */
-function showQuestion() {
+function updateToNextQuestion() {
+    // Show Question  
+    let question = questions[currentQuestion];
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    document.getElementById('displayed-question').innerHTML = currentQuestion + 1;
+}
 
-    if (currentQuestion >= questions.length) {
-        document.getElementById('end-screen').style = '';
-        document.getElementById('question-body').style = 'display: none';
-    } else {
-        let question = questions[currentQuestion];
-
-        document.getElementById('questiontext').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
-        document.getElementById('displayed-question').innerHTML = currentQuestion + 1;
-    }
+function updateProgressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;     // anzeige der prozentzahl in der progressbar
+    document.getElementById('progress-bar').style.width = `${percent}%`;   // anpassen der progressbar
 }
 
 /**
@@ -126,18 +77,27 @@ function answer(selection) {
     let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectedQuestionNumber == question['right_answer']) {
-        // console.log('Richtige Antwort!!');
+    if (rightAnswerSelected(selectedQuestionNumber)) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightQuestions++;
+        AUDIO_SUCCESS.play();
     } else {
-        // console.log('Falsche Antwort!!!');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
 
     document.getElementById('next-button').disabled = false;
 }
 
+function rightAnswerSelected(selectedQuestionNumber) {
+    return selectedQuestionNumber == question['right_answer'];
+}
+
+/**
+ * 'currentQuestion' wird um 1 erhöht
+ * der button wird wieder disabled
+ */
 function nextQuestion() {
     currentQuestion++;  //z.B. von 0 auf 1
     document.getElementById('next-button').disabled = true;
@@ -145,8 +105,10 @@ function nextQuestion() {
     showQuestion();
 }
 
+/**
+ * alle css-klassen für die farbliche anzeige werden gelöscht
+ */
 function resetAnswerButtons() {
-
     document.getElementById('answer_1').parentNode.classList.remove('bg-success');
     document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_2').parentNode.classList.remove('bg-success');
@@ -155,4 +117,14 @@ function resetAnswerButtons() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+}
+
+function restartGame() {
+    document.getElementById('header-image').src = 'assets/img/quiz.jpg';    // austausch des bildes wenn quiz neu gestartet wird
+    document.getElementById('end-screen').style = 'display: none';          // end-screen ausblenden
+    document.getElementById('question-body').style = '';                    // question-body anzeigen
+    currentQuestion = 0;
+    rightQuestions = 0;
+    init();
+
 }
